d4f9124ba92db12aa70f02401c85603d05271457
João Gois de Otoni <127162053+JGois1@users.noreply.github.com>
2023-05-25 16:30:58 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function calcularFuel(){
+
+    var gasolina = document.getElementsByClassName('gasolina').value;
+    var etanol = document.getElementsByClassName('etanol').value;
+    var result = document.getElementsByClassName('etanol');
+}
